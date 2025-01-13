a036be770440dde65c1896f25475780f182b4be1
Cédric Farcy <cfarcy.dev@gmail.com>
2025-01-13 17:05:24 +0100

    feat: 6digit otp proto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 6digit-proto
+this a pure html / javascript prototype to "one-time-password"
---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('hello, index.js');
+const otpElement = document.getElementById('otp');
+const inputs = Array.from(otpElement.children)
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    if (!/^\d$/.test(value)) {
+      e.target.value = '';
+      return;
+    }
+
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+      return;
+    }
+  });
+
+  input.addEventListener('paste', (e) => {
+    const pasteData = e.clipboardData.getData('text');
+
+    if (/^\d{6}$/.test(pasteData)) {
+      [...pasteData].forEach((digit, index) => {
+        if (inputs[index]) {
+            inputs[index].value = pasteData[index];
+        }
+
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+    }
+  });
+ 
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace' && !e.target.value && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
+
+  // select the input value. Can be change by entering a new value directly
+  input.addEventListener('focus', (e) => { e.target.select() });
+});
