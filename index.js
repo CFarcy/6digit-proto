73593daa96b8bd5249d162a3a8a24ebd0cedef32
Cédric Farcy <cfarcy.dev@gmail.com>
2025-01-14 15:28:40 +0100

    fix: populate correctly 6digit inputs

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Waiting for the sms to be received');
-  navigator.credentials.get({ otp: ["sms"] })
-
   const otpElement = document.getElementById('otp');
   const inputs = Array.from(otpElement.children)
 
@@ -45,4 +43,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // select the input value. Can be change by entering a new value directly
     input.addEventListener('focus', (e) => { e.target.select() });
   });
+
+  async function getOTPCCredential(inputs) {
+    const otp = await navigator.credentials.get({ otp: ["sms"] });
+
+    // once we got the otp code, we populate the 6 digit-inputs form
+    if (opt && otp.code) {
+      opt.code.forEach((digit, index) => {
+        inputs[index].value = digit;j
+      });
+      inputs[inputs.length - 1].focus(); // focus on the last input element
+    }
+  }
+  getOTPCCredential(inputs);
 });
