a63d64ac7a8f73111e301cdee2d8bd6c9840d144
Cédric Farcy <cfarcy.dev@gmail.com>
2025-01-14 09:55:24 +0100

    feat: use navigator interface to ask for OTP credential

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-console.log('hello, index.js');
-const otpElement = document.getElementById('otp');
-const inputs = Array.from(otpElement.children)
-inputs.forEach((input, index) => {
-  input.addEventListener('input', (e) => {
-    const value = e.target.value;
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Waiting for the sms to be received');
+  navigator.credentials.get()
 
-    if (!/^\d$/.test(value)) {
-      e.target.value = '';
-      return;
-    }
+  const otpElement = document.getElementById('otp');
+  const inputs = Array.from(otpElement.children)
 
-    if (value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-      return;
-    }
-  });
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+      const value = e.target.value;
 
-  input.addEventListener('paste', (e) => {
-    const pasteData = e.clipboardData.getData('text');
+      if (!/^\d$/.test(value)) {
+        e.target.value = '';
+        return;
+      }
 
-    if (/^\d{6}$/.test(pasteData)) {
-      [...pasteData].forEach((digit, index) => {
-        if (inputs[index]) {
+      if (value && index < inputs.length - 1) {
+        inputs[index + 1].focus();
+        return;
+      }
+    });
+
+    input.addEventListener('paste', (e) => {
+      const pasteData = e.clipboardData.getData('text');
+
+      if (/^\d{6}$/.test(pasteData)) {
+        [...pasteData].forEach((digit, index) => {
+          if (inputs[index]) {
             inputs[index].value = pasteData[index];
-        }
+          }
 
-        if (index < inputs.length - 1) {
-          inputs[index + 1].focus();
-        }
-      });
-    }
-  });
- 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace' && !e.target.value && index > 0) {
-      inputs[index - 1].focus();
-    }
-  });
+          if (index < inputs.length - 1) {
+            inputs[index + 1].focus();
+          }
+        });
+      }
+    });
 
-  // select the input value. Can be change by entering a new value directly
-  input.addEventListener('focus', (e) => { e.target.select() });
+    input.addEventListener('keydown', (e) => {
+      if (e.key === 'Backspace' && !e.target.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    });
+
+    // select the input value. Can be change by entering a new value directly
+    input.addEventListener('focus', (e) => { e.target.select() });
+  });
 });
